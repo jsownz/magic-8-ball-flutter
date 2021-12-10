d55c455eff6cc6927ce07fb3f57316d53840b542
Jason Sohl <jason@frozensoliddesigns.com>
2021-12-09 19:53:26 -0800

    flutter 8ball app for class

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: BallPage(),
+      ),
+    ));
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: SafeArea(
+        child: Center(
+          child: Ball(),
+        ),
       ),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: Image.asset('images/ball$ballNumber.png'),
+      onPressed: () {
+        setState(() {
+          ballNumber = Random().nextInt(5) + 1;
+        });
+      },
+    );
+  }
+}
